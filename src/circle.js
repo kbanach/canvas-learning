d7d9bb221cf6735cb5e712b848e80d3ddf13323e
Krystian Banach <banach.krystian@gmail.com>
2019-09-13 00:27:30 +0200

    Animate circles
    feat: add basic pulsing animation

--- src/circle.js
+++ src/circle.js
@@ -5,15 +5,20 @@ export default class Circle {
       x: 300,
       y: 300,
       r: 100,
+      defR: 100,
       startAngle: 0,
       endAngle: Math.PI * 2,
       antiClockWise: false,
     };
 
+    this.animationLen = 3;
+
   }
 
   update(time, dT) {
-    
+    this.state.r = (
+      Math.sin( Math.PI * time / (1000 * this.animationLen)) + 1  
+    ) * this.state.defR;
   }
 
   draw(ctx) {
